260e70017df0748803931dc1bbcf688400d48a9f
Miruthula90 <miruthulam@ymail.com>
2020-03-08 17:04:28 +0530

    Angular 8 basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-hello-world2';
+  username:string="Miruthula";
+  IsnotEmpty:boolean=true;
+  Isclicked=true;
+  Iscount:number=0;
+  Clickbtnarr:Array<number>=[];
+
+
+  togglepara()
+  {
+    this.Iscount+=1;
+    this.Clickbtnarr.push(this.Iscount);
+
+    this.Isclicked=!this.Isclicked;
+  }
+setusernameonclick()
+{
+  this.username="";
 }
+checkusername()
+
+{
+  if (this.username=="")
+  {
+    this.IsnotEmpty=true;
+    
+  }
+  else
+  {
+    this.IsnotEmpty=false;
+  }
+  return this.IsnotEmpty;
+}}
